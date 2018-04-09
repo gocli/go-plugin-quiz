0f62d8ad842e444191e48472e1d77fe295cdfe30
Stanislav Termosa <termosa.stanislav@gmail.com>
2018-04-09 21:23:56 +0300

    add tests for normalize-question

--- __tests__/helpers/normalize-question.spec.js
+++ __tests__/helpers/normalize-question.spec.js
@@ -9,4 +9,29 @@ describe('normalizeQuestion()', () => {
     expect(normalizeQuestion('how?', {}, []))
       .toEqual({ type: 'input', message: 'how?' })
   })
+
+  it('creates question from object', () => {
+    expect(normalizeQuestion({ message: 'how?' }, {}, []))
+      .toEqual({ type: 'input', message: 'how?' })
+  })
+
+  it('automatically set the type', () => {
+    expect(normalizeQuestion({ message: 'how?' }, {}, []).type).toBe('input')
+    expect(normalizeQuestion({ message: 'how?', choices: [] }, {}, []).type).toBe('list')
+    expect(normalizeQuestion({ message: 'how?', choices: [], multiple: true }, {}, []).type).toBe('checkbox')
+    expect(normalizeQuestion({ message: 'how?', source: [] }).type).toBe('autocomplete')
+  })
+
+  it('extend question with default', () => {
+    expect(normalizeQuestion('how?', { type: 'confirm' }, []))
+      .toEqual({ type: 'confirm', message: 'how?' })
+
+    expect(normalizeQuestion({ message: 'how?' }, { type: 'confirm' }, []))
+      .toEqual({ type: 'confirm', message: 'how?' })
+  })
+
+  it('wrap function options to call them with answers', () => {
+    const answers = []
+    expect(normalizeQuestion({ message: 'how?', default: jest.fn() }, { type: 'confirm' }, []))
+  })
 })
